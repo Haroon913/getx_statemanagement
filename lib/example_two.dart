97c34d3a87dc072438250cd279101b5fc6733175
Haroon <rajaharoon973@gmail.com>
2024-09-06 17:00:13 +0500

    getx example two

---
+++ lib/example_two.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_statemanagement/exampletwo_controller.dart';
+class ExampleTwo extends StatefulWidget {
+  const ExampleTwo({super.key});
+
+  @override
+  State<ExampleTwo> createState() => _ExampleTwoState();
+}
+
+class _ExampleTwoState extends State<ExampleTwo> {
+  final ExampletwoController exampletwoController=Get.put(ExampletwoController());
+
+
+  @override
+  Widget build(BuildContext context) {
+    print('rebuild');
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text('GetX Tutorials'),
+      ),
+      body: Column(
+        children: [
+          Obx(()=>  Container(
+            height: 200,
+            width: 200,
+            color: Colors.orange.withOpacity(exampletwoController.opacity.value),
+          ),),
+          Obx(()=>SliderTheme(
+            data: SliderTheme.of(context).copyWith(
+              thumbColor: Colors.orange,
+              overlayColor: Colors.black,
+              activeTrackColor: Colors.black,
+            ),
+            child: Slider(
+                value: exampletwoController.opacity.value, onChanged: (value){
+              exampletwoController.setOpacity(value);
+            }),
+          ))
+
+
+        ],
+      ),
+    );
+  }
+}
