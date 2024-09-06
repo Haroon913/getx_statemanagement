79a013d01e087a762f36079d0ac96dc6ac05341b
Haroon <rajaharoon973@gmail.com>
2024-09-06 16:20:36 +0500

    counter example getx

---
+++ lib/homescreen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx_statemanagement/counter_controller.dart';
+class Homescreen extends StatefulWidget {
+  const Homescreen({super.key});
+
+  @override
+  State<Homescreen> createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  final CounterController controller=Get.put(CounterController());
+  int x=20;
+  @override
+  Widget build(BuildContext context) {
+    print('rebuild');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GetX Tutorials'),
+      ),
+      body: Center(child:
+     Obx((){
+       return Text(controller.counter.toString(),style: TextStyle(fontSize: 50),);
+     })
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          controller.incrementCounter();
+        },
+      ),
+    );
+  }
+}
